950abfffc8e16bdf0b487a7f543ff8090d9606e9
kiohunter12 <kioarce12@gmail.com>
2024-11-06 17:54:31 -0500

    gg

--- ms-venta/src/main/java/com/example/msventa/feign/LibroFeign.java
+++ ms-venta/src/main/java/com/example/msventa/feign/LibroFeign.java
@@ -1,6 +1,7 @@
 package com.example.msventa.feign;
 
 import com.example.msventa.dto.LibroDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name="ms-book-service")
 public interface LibroFeign {
+    @CircuitBreaker(name = "libroListarPorIdCB", fallbackMethod = "fallbackLibro")
     @GetMapping("/libros/{id}")
     ResponseEntity<LibroDto>listarLibro(@PathVariable(required = true) Integer id);
     @PutMapping("/libros/{id}/stock")
     ResponseEntity<Void> actualizarStock(@PathVariable Integer id, @RequestParam Integer cantidad);
+    default ResponseEntity<LibroDto> fallbackLibro(Integer id, Exception e) {
+        LibroDto libroDto = new LibroDto();
+        libroDto.setId(9000000);
+        return ResponseEntity.ok(libroDto);
+    }
 
 }
